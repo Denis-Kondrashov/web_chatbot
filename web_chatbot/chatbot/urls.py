84b1ad52c601d908b2e6a42b549729ab436579db
DenisK5 <denis.kondrashov1@gmail.com>
2024-12-25 22:52:16 +0300

    fix: решена проблема с выводом ответа

--- web_chatbot/chatbot/urls.py
+++ web_chatbot/chatbot/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import home, ask_question
+from .views import home, answer
 
 urlpatterns = [
     path('', home, name='home'),
-    path('ask/', ask_question, name='ask_question'),
+    path('answer/', answer, name='answer'),
 ]
