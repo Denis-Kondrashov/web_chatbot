91aaeef315e2c73c19f676284e8b7fc10be2cfe8
DenisK5 <denis.kondrashov1@gmail.com>
2025-02-20 20:47:43 +0300

    feat: подключена  локально база данных PostgreSQL через Docker

--- web_chatbot/chatbot/models.py
+++ web_chatbot/chatbot/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Cat(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Кличка")
+    age = models.IntegerField(verbose_name="Возраст")
+    color = models.CharField(max_length=30, verbose_name="Цвет")
+
+    def __str__(self):
+        return f"{self.name} ({self.color}, {self.age} лет)"
--- web_chatbot/web_chatbot/settings.py
+++ web_chatbot/web_chatbot/settings.py
@@ -9,12 +9,21 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+import os
+from dotenv import load_dotenv
+
 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+
+
+# print("BASE_DIR:", BASE_DIR)
+# print("Путь к .env:", os.path.join(BASE_DIR, '.env'))
+# print("DB_PASSWORD:", os.getenv("DB_PASSWORD"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -76,8 +85,12 @@ WSGI_APPLICATION = 'web_chatbot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'postgres'),
+        'USER': os.getenv('DB_USER', 'postgres'),
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', 'localhost'),  # Имя контейнера PostgreSQL
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
