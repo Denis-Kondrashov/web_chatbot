bcd6af3042f3f2615a6b3504c4256d9973566711
DenisK5 <denis.kondrashov1@gmail.com>
2025-02-22 12:30:18 +0300

    feat/fix: переписаны models и json, исправлена ошибка с загрузкой данных из .env

--- web_chatbot/chatbot/models.py
+++ web_chatbot/chatbot/models.py
@@ -1,10 +1,21 @@
 from django.db import models
 
 
+class Owner(models.Model):
+    first_name = models.CharField(max_length=50, verbose_name="Имя")
+    last_name = models.CharField(max_length=50, verbose_name="Фамилия")
+    phone_number = models.CharField(max_length=15, verbose_name="Номер телефона", blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
 class Cat(models.Model):
     name = models.CharField(max_length=50, verbose_name="Кличка")
     age = models.IntegerField(verbose_name="Возраст")
     color = models.CharField(max_length=30, verbose_name="Цвет")
+    owner = models.ForeignKey(Owner, on_delete=models.SET_NULL, verbose_name="Владелец", null=True, blank=True)
 
     def __str__(self):
-        return f"{self.name} ({self.color}, {self.age} лет)"
+        return f"{self.name} ({self.color}, {self.age} лет) {self.owner}"
+
--- web_chatbot/web_chatbot/settings.py
+++ web_chatbot/web_chatbot/settings.py
@@ -18,7 +18,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(os.path.join(BASE_DIR, '.env'))
+load_dotenv(os.path.join(BASE_DIR.parent, '.env'))
 
 
 # print("BASE_DIR:", BASE_DIR)
