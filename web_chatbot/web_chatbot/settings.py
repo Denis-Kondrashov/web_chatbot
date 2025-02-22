0bf8fabcd3ba10fa243f40ce2ea248142a80af73
DenisK5 <denis.kondrashov1@gmail.com>
2025-02-22 13:42:22 +0300

    feat: database test

--- web_chatbot/web_chatbot/settings.py
+++ web_chatbot/web_chatbot/settings.py
@@ -11,9 +11,15 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+from dotenv import load_dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(os.path.join(BASE_DIR.parent, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
@@ -76,8 +82,12 @@ WSGI_APPLICATION = 'web_chatbot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'postgres'),
+        'USER': os.getenv('DB_USER', 'postgres'),
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', 'localhost'),  # Имя контейнера PostgreSQL
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
